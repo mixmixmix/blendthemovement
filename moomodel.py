26c37712d123d076b7bd0f475af8e91615d04e80
M Kunde <mikolaj.kundegorski@gmail.com>
2021-05-18 20:07:40 +0100

    really poorly but first attempt

---
+++ moomodel.py
@@ -0,0 +1,106 @@
+import math
+import numpy as np
+
+
+class Mooveemodel:
+    def __init__(self, init_pos, mu_s, sigma_speed, sigma_angular_velocity, theta_speed, theta_angular_velocity, border=None, side=None):
+        self.mu = np.array([mu_s,0.])
+        self.theta = np.array([theta_speed,theta_angular_velocity])
+        self.sigma = np.array([sigma_speed,sigma_angular_velocity])
+        self.v = np.array([0,0,0])
+        self.dt = np.ones(3)
+        self.rng = np.random.default_rng()
+        self.pos = np.array(init_pos)
+        self.angle = 30. #alpha angle
+        self.os = np.array(self.mu)
+        self.s = 0
+        self.border = border
+
+        if border:
+            if side:
+                self.side = side
+            if not side:
+                print(f'You need to provide argument \'side\' for the border conditions {borderMethod}')
+
+        if border=='normal':
+            self.bdist=0.5 #half the power of border rejection
+
+        self.updateSpeed()
+
+    #The "normal" boundary condition is a hack on the output angle of the movement model, it accelerates the turn of angle without feeding in to the model
+    def borderRepulsionVector(self):
+        dist_low = -self.side + self.pos[1]
+        dist_high = self.side - self.pos[1]
+        dist_left = -self.side + self.pos[0]
+        dist_right = self.side - self.pos[0]
+
+        mag = 0.1
+        # print(f' The distances are {dist_low} {dist_high}, {dist_left}, {dist_right}')
+        force_low = 1 / (1+math.exp(mag*(dist_low-self.bdist)))
+        force_high = 1 / (1+math.exp(mag*(dist_high-self.bdist)))
+        force_left = 1 / (1+math.exp(mag*(dist_left-self.bdist)))
+        force_right = 1 / (1+math.exp(mag*(dist_right-self.bdist)))
+        # print(f' The forceances are {force_low} {force_high}, {force_left}, {force_right}')
+
+        #add vectors:
+        return np.array([force_left-force_right, force_low-force_high])
+
+
+    def updateSpeed(self):
+        os1 = self.os
+        mu1 = self.mu
+        theta1 = self.theta
+        dt1 = self.dt[0:2]
+        sigma1 = self.sigma
+        rng1 = self.rng
+
+        self.os = (os1
+            + theta1 * (mu1 - os1) * dt1
+            + sigma1 * rng1.normal(0,np.sqrt(dt1),2)
+        )
+
+        self.angle = self.angle + self.os[1] * dt1[1] #angle of X/Y plane
+        #self.s = np.log1p(np.exp(self.os[0])) #softplus cause it to get stuck in 0.
+
+        inv=np.array([0.,0.])#this must be float otherwise python gets messed up real quick
+        ouv=np.array([0.,0.])
+
+        # print(f'pre angle {self.angle}')
+        if self.border=='normal':
+            inv[0] = np.cos(self.angle)
+            inv[1] = np.sin(self.angle)
+            brv = self.borderRepulsionVector()
+            # print(f'Repulsion! {brv}')
+            ouv [0] = inv[0] + brv[0]
+            ouv [1] = inv[1] + brv[1]
+            # print(f'vector inv: {inv}')
+            # print(f'vector brv: {brv}')
+            # print(f'vector ouv: {ouv}')
+            self.angle = np.arctan2(ouv[1],ouv[0])
+
+        # print(f'post angle {self.angle}')
+
+        self.s = abs(self.os[0])
+        self.v[0] = self.s*np.cos(self.angle)
+        self.v[1] = self.s*np.sin(self.angle)
+        self.v[2] = 0 #no movement on z-axis for now...
+
+        return self.v
+
+    def updatePosition(self):
+        new_pos = self.pos + self.v * self.dt
+        if not self.border:
+            self.pos = new_pos
+            return self.pos
+        elif self.border=='periodic':
+            self.pos = new_pos % self.side
+            is_same_panel = True if np.all(new_pos == self.pos) else False
+            return self.pos, is_same_panel
+        elif self.border=='normal':
+            self.pos = new_pos
+            return self.pos
+
+
+    #2D only
+    def getDirection(self):
+        return np.degrees(np.arctan2(self.v[1],self.v[0]))
---
+++ new_sti.py
@@ -0,0 +1,94 @@
+import math
+import time
+import numpy as np
+from collections import deque
+import numpy as np #remember to add to sys.path proper python libs
+import bpy
+from mathutils import Vector
+import moomodel
+#test
+
+"""
+Updates position of all Zwierzaks.
+They generally like to cluster, if they are suitably far away
+"""
+def updateSticklePosition(zwk,mm):
+
+    zwk.prev_pos = zwk.pos
+
+    cur_v = mm.updateSpeed()
+    cur_pos = mm.updatePosition()
+
+    zwk.angle[0] = mm.getDirection()
+
+    # print(f'We experience {cur_v} and {cur_pos}')
+
+    # zwk.pos = [int(cur_pos[0]),int(cur_pos[1]),int(cur_pos[2])] #for blender comment this line!!! HACK
+    zwk.pos = cur_pos
+    return zwk
+
+
+class Stickle:
+    def __init__(self, zwkid, init_pos):
+        self.id = zwkid
+        self.init_pos=init_pos
+        self.pos=init_pos
+        self.prev_pos=init_pos
+        self.angle = [0,0] #angle[0] alpha, angle[1] beta
+        self.islong = 30 #half of width and height as opencv ellipses measurements defined
+        self.iswide = 10
+
+        #unusual numbers to encourage program loudly crashing
+        self.topleft = -111
+        self.bottomright = -111
+        self.topleft_prev = -111
+        self.bottomright_prev = -111
+
+
+# useful shortcut
+scene = bpy.context.scene
+
+side = 5
+
+number_of_frame = 0
+scene.frame_set(number_of_frame)
+ani = bpy.data.objects['hemingway']
+ani.rotation_mode = 'XYZ'
+ani.location=(0,0,0)
+ani.rotation_euler = (np.pi/2, 0, 0) 
+ani.keyframe_insert(data_path="location", index=-1)
+ani.keyframe_insert(data_path="rotation_euler", index=-1)
+number_of_frame += 1
+
+init_pos = [0,0,0]
+stickle = Stickle('s',init_pos)
+
+
+mu_s  = 0
+#sigma_speed = 4
+#sigma_angular_velocity = 0.1
+#theta_speed = 0.1
+#theta_angular_velocity = 0.1
+sigma_speed = 0.3
+theta_speed = 0.001
+sigma_angular_velocity = 1
+theta_angular_velocity = 0.01
+
+
+mm = moomodel.Mooveemodel(init_pos, mu_s, sigma_speed,sigma_angular_velocity,theta_speed, theta_angular_velocity, border='normal',side=side)
+
+
+for it in range(1,2500,10):
+    scene.frame_set(number_of_frame)
+    stickle = updateSticklePosition(stickle,mm)
+    # alf = handleColisions(alf,borders,alfs)
+    ani.location = (stickle.pos[1],0,-stickle.pos[0])
+    aa = stickle.angle[0]
+    #blender_angle = 2 * alf.angle / np.pi
+    blender_angle = np.radians(aa) 
+    ani.rotation_euler = (np.pi/2, 0, blender_angle) 
+    ani.keyframe_insert(data_path="location", frame=it)
+    ani.keyframe_insert(data_path="rotation_euler", frame = it)
+    #print([alf.x_pos,alf.y_pos])
+    print(f'frame {number_of_frame}, we got alf angle of {aa} and for blender it is  {blender_angle}')
+    number_of_frame += 10
